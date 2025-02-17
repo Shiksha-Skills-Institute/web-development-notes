8c52b60fb33c1c0ff9e1505555c83a3e1ef3c4c1
Ronak Vanpariya <ronak.vanpariya@multidots.com>
2025-02-17 11:44:48 +0530

    Updated code for Basic

--- Javascript/basic/script.js
+++ Javascript/basic/script.js
@@ -1,1 +1,26 @@
-alert("Hello");
+// window.alert("Hello");
+
+var headingElement = document.getElementsByTagName('h1');
+console.log(headingElement.length);
+
+var idElement = document.getElementById('some-id');
+
+
+console.log(idElement)
+
+console.log(idElement.length)
+
+var classElement = document.getElementsByClassName("some-class")
+console.log('Class Element', classElement)
+console.log('Class Length', classElement.length);
+
+var queryClass = document.querySelector('.some-class')
+
+console.log('Query Element', queryClass)
+console.log('Query Length', queryClass.length);
+
+var queryAllClass = document.querySelectorAll('.some-class')
+
+console.log('QueryAll Element', queryAllClass)
+console.log('QueryAll Length', queryClass.length);
+
