e15a0f2e4e538b0c4801dc1aa62f40e02216527e
Ronak J Vanpariya <26689210+vanpariyar@users.noreply.github.com>
2025-03-17 06:47:08 +0000

    Update the JS course

---
+++ Javascript/5-loops/while.js
@@ -0,0 +1,19 @@
+var i = 0;
+while( i < 5 ) {
+    console.log("Shiksha Skills Institute")
+    if( i === 2){
+        continue;
+    }
+    i++;
+}
+
+console.log(`_________________`);
+
+var i = 0;
+do{
+    console.log("Shiksha Skills Institute")
+    if( i === 2){
+        break;
+    }
+    i++;
+}while( i < 5 );
