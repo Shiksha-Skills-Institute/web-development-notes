15f99ed7f04ea06208961b9f4376927f105f3e19
Ronak J Vanpariya <26689210+vanpariyar@users.noreply.github.com>
2025-04-11 05:56:40 +0000

    Push

--- Javascript/6-functions/learn/timefunctions.js
+++ Javascript/6-functions/learn/timefunctions.js
@@ -1,2 +1,19 @@
 //setInterval
 //setTimeout
+
+var number = 1;
+var innerP = document.querySelector('section div p');
+function addNewNumber(){
+    const tempP = document.createElement("p");
+    tempP.innerHTML = ++number;
+    innerP.insertAdjacentElement('beforeend', tempP)
+}
+
+// addNewNumber();
+const intervalID = setInterval( addNewNumber, 1000);
+
+function stopAddingNewNumbers(){
+    clearInterval(intervalID);
+}
+
+setTimeout( stopAddingNewNumbers, 9000 );
